219d1bfafb6c025d9b1ab0dac4874077ca5329d4
Sumanth Tatipamula <sumanthtatipamula999@gmail.com>
2022-03-30 04:47:35 +0530

    Week - 3 Initial Setup

---
+++ week-3/confusion/src/components/DishDetailComponent.js
@@ -0,0 +1,75 @@
+import {
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	CardTitle,
+	Breadcrumb,
+	BreadcrumbItem,
+} from 'reactstrap';
+
+import { Link } from 'react-router-dom';
+
+function RenderDish({ dish }) {
+	return (
+		<div className='col-12 col-md-5 m-1'>
+			<Card>
+				<CardImg width='100%' src={dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>
+			</Card>
+		</div>
+	);
+}
+
+function RenderComments({ comments }) {
+	const commentsList = comments.map((comment) => {
+		return (
+			<ul key={comment.id} className='list-unstyled'>
+				<li>{comment.comment}</li>
+				<li>
+					-- {comment.author}
+					{', '}
+					{new Intl.DateTimeFormat('en-US', {
+						year: 'numeric',
+						month: 'short',
+						day: '2-digit',
+					}).format(new Date(Date.parse(comment.date)))}
+				</li>
+			</ul>
+		);
+	});
+	return (
+		<div className='col-12 col-md-5 m-1'>
+			<h4>Comments</h4>
+			{commentsList}
+		</div>
+	);
+}
+
+const DishDetail = (props) => {
+	if (props.dish == null) return <div></div>;
+	return (
+		<div className='container'>
+			<div className='row'>
+				<Breadcrumb>
+					<BreadcrumbItem>
+						<Link to='/menu'>Menu</Link>
+					</BreadcrumbItem>
+					<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+				</Breadcrumb>
+				<div className='col-12'>
+					<h3>{props.dish.name}</h3>
+					<hr />
+				</div>
+			</div>
+			<div className='row'>
+				<RenderDish dish={props.dish} />
+				<RenderComments comments={props.comments} />
+			</div>
+		</div>
+	);
+};
+export default DishDetail;
---
+++ week-3/confusion/src/components/MainComponent.js
@@ -0,0 +1,78 @@
+import { Component } from 'react';
+import MenuComponent from './MenuComponent';
+import DishDetail from './DishDetailComponent';
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import About from './AboutComponent';
+class Main extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dishes: DISHES,
+			comments: COMMENTS,
+			promotions: PROMOTIONS,
+			leaders: LEADERS,
+		};
+	}
+	render() {
+		const HomePage = () => {
+			return (
+				<Home
+					dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+					promotion={
+						this.state.promotions.filter((promotion) => promotion.featured)[0]
+					}
+					leader={
+						this.state.leaders.filter((leader) => leader.featured)[0]
+					}></Home>
+			);
+		};
+		const DishWithId = ({ match, location, history }) => {
+			return (
+				<DishDetail
+					dish={
+						this.state.dishes.filter((dish) => {
+							return dish.id === parseInt(match.params.id, 10);
+						})[0]
+					}
+					comments={this.state.comments.filter((comment) => {
+						return comment.dishId === parseInt(match.params.id, 10);
+					})}></DishDetail>
+			);
+		};
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route path='/home' component={HomePage} />
+					<Route
+						path='/aboutus'
+						component={() => {
+							return <About leaders={this.state.leaders} />;
+						}}
+					/>
+					<Route
+						exact
+						path='/menu'
+						component={() => (
+							<MenuComponent dishes={this.state.dishes}></MenuComponent>
+						)}
+					/>
+					<Route path='/menu/:id' component={DishWithId} />
+					<Route exact path='/contactus' component={Contact} />
+					<Redirect to='/home' />
+				</Switch>
+				<Footer />
+			</div>
+		);
+	}
+}
+
+export default Main;
