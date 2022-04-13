fba19e19bce90d0dcf934ee5fb50f53ed4b14fd9
Sumanth Tatipamula <sumanthtatipamula999@gmail.com>
2022-04-14 04:53:11 +0530

    Fetch Handling Errors

--- week-4/confusion/src/redux/ActionCreators.js
+++ week-4/confusion/src/redux/ActionCreators.js
@@ -16,10 +16,29 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => {
 	dispatch(dishesLoading(true));
 	return fetch(baseUrl + 'dishes')
+		.then(
+			(response) => {
+				if (response.ok) {
+					return response;
+				} else {
+					var error = new Error(
+						'Error ' + response.status + ': ' + response.statusText
+					);
+					error.response = response;
+					throw error;
+				}
+			},
+			(error) => {
+				//When we dont hear from server
+				var errmess = new Error(error.message);
+				throw errmess;
+			}
+		)
 		.then((response) => response.json())
 		.then((dishes) => {
 			dispatch(addDishes(dishes));
-		});
+		})
+		.catch((error) => dispatch(dishesFailed(error.message)));
 	// setTimeout(() => {
 	// 	dispatch(addDishes(DISHES));
 	// }, 2000);
@@ -50,19 +69,55 @@ export const addComments = (comments) => ({
 
 export const fetchComments = () => (dispatch) => {
 	return fetch(baseUrl + 'comments')
+		.then(
+			(response) => {
+				if (response.ok) {
+					return response;
+				} else {
+					var error = new Error(
+						'Error ' + response.status + ': ' + response.statusText
+					);
+					error.response = response;
+					throw error;
+				}
+			},
+			(error) => {
+				var errmess = new Error(error.message);
+				throw errmess;
+			}
+		)
 		.then((response) => response.json())
 		.then((comments) => {
 			dispatch(addComments(comments));
-		});
+		})
+		.catch((error) => dispatch(commentsFailed(error.message)));
 };
 
 export const fetchPromos = () => (dispatch) => {
 	dispatch(promosLoading(true));
 	return fetch(baseUrl + 'promotions')
+		.then(
+			(response) => {
+				if (response.ok) {
+					return response;
+				} else {
+					var error = new Error(
+						'Error ' + response.status + ': ' + response.statusText
+					);
+					error.response = response;
+					throw error;
+				}
+			},
+			(error) => {
+				var errmess = new Error(error.message);
+				throw errmess;
+			}
+		)
 		.then((response) => response.json())
 		.then((promos) => {
 			dispatch(addPromos(promos));
-		});
+		})
+		.catch((error) => dispatch(promosFailed(error.message)));
 };
 
 export const promosLoading = () => ({
